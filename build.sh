af3233e9298caa9433fae01396ad55ed114ab158
Vansh Bhatnagar <115162664+Vansh41104@users.noreply.github.com>
2025-03-29 12:29:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID vansh41104/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID vansh41104/$JOB_NAME:latest
 
-docker push jacksneel/$JOB_NAME:$BUILD_ID
+docker push vansh41104/$JOB_NAME:$BUILD_ID
 
-docker push jacksneel/$JOB_NAME:latest
+docker push vansh41104/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID vansh41104/$JOB_NAME:$BUILD_ID vansh41104/$JOB_NAME:latest
